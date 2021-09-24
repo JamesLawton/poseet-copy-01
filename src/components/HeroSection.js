d031fafa988586b66d20aa208115079d6f36c20e
Stackbit Code Editor <projects@stackbit.com>
2021-09-24 07:34:52 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -56,6 +56,13 @@ export default class HeroSection extends React.Component {
                                     'cell-md-7': isHorizontal && mediaWidth === 'sixty'
                                 })}
                             >
+                                                            <div
+                                className={classNames('hero__media', 'my-2', 'cell-8', {
+                                    'cell-md-4': isHorizontal && mediaWidth === 'fourty',
+                                    'cell-md-6': isHorizontal && mediaWidth === 'fifty',
+                                    'cell-md-7': isHorizontal && mediaWidth === 'sixty'
+                                })}
+                            >
                                 {videoEmbed ? htmlToReact(videoEmbed)
                                     : <img
                                             src={withPrefix(image)}
