82fb8b52aa14a2218e9e62104bac5a4c5f95bbeb
Stackbit Code Editor <projects@stackbit.com>
2021-09-24 08:13:08 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -75,7 +75,7 @@ export default class HeroSection extends React.Component {
                             
                         )}
                                                 <div
-                        className={classNames('hero__media', 'my-2', 'cell-2'
+                        className={classNames('hero__media', 'my-2', 'cell-3'
                         )}
                         >
                         </div>
